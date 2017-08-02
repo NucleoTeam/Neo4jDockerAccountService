1466341bdd04c56a9772c42997bed736b550db3c
nathanieldavidson <ndavidson@csumb.edu>
2017-08-02 14:45:06 -0700

    redis dependencies

--- src/main/java/com/synload/accountControl/SessionRedisConfig.java
+++ src/main/java/com/synload/accountControl/SessionRedisConfig.java
@@ -1,13 +1,8 @@
 package com.synload.accountControl;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 public class SessionRedisConfig {
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
+
 }
